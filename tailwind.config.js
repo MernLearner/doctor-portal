3cd7786a158f518d1b064519e33637aec9e4683d
MernLearner <mern.learner@gmail.com>
2022-07-30 00:22:49 +0600

    hero section done

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,21 +5,21 @@ module.exports = {
   ],
   theme: {
     extend: {},
-    daisyui: {
-      themes: [
-        {
-          mytheme: {
-            primary: "#19D3AE",
-            secondary: "#f6d860",
-            accent: "#37cdbe",
-            neutral: "#3d4451",
-            "base-100": "#ffffff",
-          },
+  },
+  daisyui: {
+    themes: [
+      {
+        doctortheme: {
+          primary: "#0FCFEC",
+          secondary: "#19D3AE",
+          accent: "#3A4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
         },
-        "dark",
-        "cupcake",
-      ],
-    },
+      },
+      "dark",
+      "cupcake",
+    ],
   },
   plugins: [require("daisyui")],
 }
